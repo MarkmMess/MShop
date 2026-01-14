629476355d06aa6750c8a081e77e5e101d6306cd
MarkmMess <mark.markovsky1@gmail.com>
2026-01-14 20:55:07 +0300

    create profile for user

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,14 +1,8 @@
-__all__ = (
-    "Base",
-    "Product",
-    "db_helper",
-    "DataBaseHelper",
-    "User",
-    "Post",
-)
+__all__ = ("Base", "Product", "db_helper", "DataBaseHelper", "User", "Post", "profile")
 
 from .product import Product
 from .base import Base
 from .db_helper import db_helper, DataBaseHelper
 from .user import User
 from .post import Post
+from .profile import Profile
--- src/models/user.py
+++ src/models/user.py
@@ -1,14 +1,17 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import String, Boolean, DateTime
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
 
 if TYPE_CHECKING:
     from .post import Post
+    from .profile import Profile
 
 
 class User(Base):
     username: Mapped[str] = mapped_column(String(20), unique=True)
+
     posts: Mapped[list["Post"]] = relationship(back_populates="user")
+    pofile: Mapped["Profile"] = relationship(back_populates="profile")
