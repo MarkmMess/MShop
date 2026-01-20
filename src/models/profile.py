0abda3dd5b08e6400062e4f0298de16a5dd41094
MarkmMess <mark.markovsky1@gmail.com>
2026-01-20 20:59:53 +0300

    add magic method str and repr to Profile

--- src/models/profile.py
+++ src/models/profile.py
@@ -12,3 +12,9 @@ class Profile(UserRelationMixin, Base):
     first_name: Mapped[str | None] = mapped_column(String[40])
     last_name: Mapped[str | None] = mapped_column(String[40])
     bio: Mapped[str | None]
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(first_name:{self.first_name!r}, last_name:{self.last_name!r})"
+
+    def __repr__(self):
+        return str(self)
