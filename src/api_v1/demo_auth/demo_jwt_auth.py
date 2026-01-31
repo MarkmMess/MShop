96633fb2be19f00d655f75a46715b32f9b2e510e
MarkmMess <mark.markovsky1@gmail.com>
2026-01-31 23:58:12 +0300

    add log through oauth2 bearer

--- src/api_v1/demo_auth/demo_jwt_auth.py
+++ src/api_v1/demo_auth/demo_jwt_auth.py
@@ -1,9 +1,17 @@
-from fastapi import APIRouter, Depends, Form, HTTPException
+from fastapi import APIRouter, Depends, Form, HTTPException, status
+from fastapi.security import (
+    HTTPBearer,
+    HTTPAuthorizationCredentials,
+    OAuth2PasswordBearer,
+)
 from pydantic import BaseModel
-from starlette import status
+from jose import JWTError
 
 from src.api_v1.users.schemas import UserSchema
 from src.auth import utils as auth_utils
+
+# http_bearer = HTTPBearer()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/demo_jwt_auth/jwt/login/")
 
 
 class TokenInfo(BaseModel):
@@ -47,6 +55,45 @@ def validate_auth_user(
     raise exc
 
 
+def get_current_token_payload(
+    # cred: HTTPAuthorizationCredentials = Depends(http_bearer),
+    token: str = Depends(oauth2_scheme),
+) -> dict:
+    # token = cred.credentials
+    try:
+        payload = auth_utils.decode_jwt(
+            token,
+        )
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"invalid token error",
+        )
+    return payload
+
+
+def get_current_auth_user(
+    payload: dict = Depends(get_current_token_payload),
+) -> UserSchema:
+    username: str | None = payload.get("sub")
+    if user := users_db.get(username):
+        return user
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+    )
+
+
+def get_current_active_auth_user(
+    user: UserSchema = Depends(get_current_auth_user),
+):
+    if user.active:
+        return user
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="Inactive user",
+    )
+
+
 @router.post("/login/", response_model=TokenInfo)
 def auth_user_jwt(
     user: UserSchema = Depends(validate_auth_user),
@@ -62,3 +109,16 @@ def auth_user_jwt(
         access_token=token,
         token_type="Bearer",
     )
+
+
+@router.get("/user/i/")
+def auth_user_check_info(
+    payload: dict = Depends(get_current_token_payload),
+    user: UserSchema = Depends(get_current_active_auth_user),
+):
+    iat = payload.get("iat")
+    return {
+        "username": user.username,
+        "email": user.email,
+        "logged in at": iat,
+    }
