33b1413ad4d20959ef019e97c56c4352da1b6ede
MarkmMess <mark.markovsky1@gmail.com>
2026-02-01 21:45:17 +0300

    use run config in main

--- src/main.py
+++ src/main.py
@@ -21,4 +21,9 @@ async def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run(
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
