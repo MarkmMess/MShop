7428c531beffa9824f918fdb2d34c66c5373caa0
MarkmMess <mark.markovsky1@gmail.com>
2026-02-02 04:40:13 +0300

    demo edit for post model

--- src/models/post.py
+++ src/models/post.py
@@ -1,11 +1,13 @@
 from sqlalchemy import String, Text
 from sqlalchemy.orm import Mapped, mapped_column
-from .mixins import UserRelationMixin
+
+# from .mixins import UserRelationMixin
 
 from .base import Base
 
 
-class Post(UserRelationMixin, Base):
+# class Post(UserRelationMixin, Base):
+class Post(Base):
     _user_back_populates = "posts"
     title: Mapped[str] = mapped_column(String(100))
     body: Mapped[str] = mapped_column(
