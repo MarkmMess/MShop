2e5f340109827af95efc56ff89b01e6b8931f9ad
MarkmMess <mark.markovsky1@gmail.com>
2026-02-04 21:46:58 +0300

    archive files

--- src/models/order_product_association.py
+++ src/models/order_product_association.py
@@ -1,29 +1,29 @@
-from typing import TYPE_CHECKING
-
-from sqlalchemy import ForeignKey, UniqueConstraint
-from sqlalchemy.orm import mapped_column, Mapped, relationship
-
-from .base import Base
-
-if TYPE_CHECKING:
-    from . import Order, Product
-
-
-class OrderProductAssociation(Base):
-    __tablename__ = "order_product_association"
-    __table_args__ = (
-        UniqueConstraint(
-            "order_id",
-            "product_id",
-            name="idx_order_product_association",
-        ),
-    )
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"))
-    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"))
-    count: Mapped[int] = mapped_column(default=1, server_default="1")
-    unit_price: Mapped[int] = mapped_column(default=0, server_default="0")
-
-    order: Mapped["Order"] = relationship(back_populates="products_details")
-    product: Mapped["Product"] = relationship(back_populates="orders_details")
+# from typing import TYPE_CHECKING
+#
+# from sqlalchemy import ForeignKey, UniqueConstraint
+# from sqlalchemy.orm import mapped_column, Mapped, relationship
+#
+# from .base import Base
+#
+# if TYPE_CHECKING:
+#     from . import Order, Product
+#
+#
+# class OrderProductAssociation(Base):
+#     __tablename__ = "order_product_association"
+#     __table_args__ = (
+#         UniqueConstraint(
+#             "order_id",
+#             "product_id",
+#             name="idx_order_product_association",
+#         ),
+#     )
+#
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"))
+#     product_id: Mapped[int] = mapped_column(ForeignKey("products.id"))
+#     count: Mapped[int] = mapped_column(default=1, server_default="1")
+#     unit_price: Mapped[int] = mapped_column(default=0, server_default="0")
+#
+#     order: Mapped["Order"] = relationship(back_populates="products_details")
+#     product: Mapped["Product"] = relationship(back_populates="orders_details")
