1563ac7e1db9a3dc457cf5d58a87b1a5924c4806
MarkmMess <mark.markovsky1@gmail.com>
2026-02-04 21:49:10 +0300

    new user-profile relations

--- src/models/profile.py
+++ src/models/profile.py
@@ -1,19 +1,25 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 # from .mixins import UserRelationMixin
 
 from .base import Base
 
+if TYPE_CHECKING:
+    from .user import User
+
 
 # class Profile(UserRelationMixin, Base):
 class Profile(Base):
     _user_id_unique = True
-    _user_back_populates = "profile"
 
     first_name: Mapped[str | None] = mapped_column(String[40])
     last_name: Mapped[str | None] = mapped_column(String[40])
     bio: Mapped[str | None]
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True)
+    user: Mapped["User"] = relationship(back_populates="profiles")
 
     def __str__(self):
         return f"{self.__class__.__name__}(first_name:{self.first_name!r}, last_name:{self.last_name!r})"
--- src/models/user.py
+++ src/models/user.py
@@ -6,7 +6,6 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .base import Base
 
 if TYPE_CHECKING:
-    from .post import Post
     from .profile import Profile
     from .product import Product
 
@@ -14,9 +13,8 @@ if TYPE_CHECKING:
 class User(Base):
     username: Mapped[str] = mapped_column(String(20), unique=True)
     hashed_password: Mapped[str] = mapped_column(String(128), unique=True)
-    posts: Mapped[list["Post"]] = relationship(back_populates="users")
-    profile: Mapped["Profile"] = relationship(back_populates="users")
 
+    profile: Mapped["Profile"] = relationship(back_populates="users")
     products: Mapped[list["Product"]] = relationship(back_populates="users")
 
     def __str__(self):
