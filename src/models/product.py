d2bda2428995929cf035fa5657cfd23c56e16e03
MarkmMess <mark.markovsky1@gmail.com>
2026-02-05 02:17:08 +0300

    bag fixed

--- src/models/product.py
+++ src/models/product.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timezone
 
-# from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING
 
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, relationship, mapped_column
@@ -10,29 +10,17 @@ from .base import Base
 from .user import User
 
 
-#
-# if TYPE_CHECKING:
-#     from .order import Order
-#     from .order_product_association import OrderProductAssociation
-#
+if TYPE_CHECKING:
+    from .user import User
 
 
-# class Product(UserRelationMixin, Base):
 class Product(Base):
     name: Mapped[str]
     description: Mapped[str]
     price: Mapped[float]
     created_at: Mapped[datetime] = mapped_column(default=datetime.now(timezone.utc))
 
-    username: Mapped[str] = mapped_column(
-        ForeignKey("users.username", ondelete="CASCADE")
+    user_id: Mapped[str] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=False
     )
     user: Mapped["User"] = relationship(back_populates="products")
-    # orders: Mapped[list["Order"]] = relationship(
-    #     secondary="order_product_association",
-    #     back_populates="products",
-    # )
-
-    # orders_details: Mapped[list["OrderProductAssociation"]] = relationship(
-    #     back_populates="product",
-    # )
