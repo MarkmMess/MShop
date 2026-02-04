fe4f489e71329d57e1a6a1238d9b72bf068ba093
MarkmMess <mark.markovsky1@gmail.com>
2026-02-04 21:51:28 +0300

    delete unused models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,17 +4,18 @@ __all__ = (
     "db_helper",
     "DataBaseHelper",
     "User",
-    "Post",
     "Profile",
-    "Order",
-    "OrderProductAssociation",
+    # "Post",
+    # "Order",
+    # "OrderProductAssociation",
 )
 
 from .product import Product
 from .base import Base
 from .db_helper import db_helper, DataBaseHelper
 from .user import User
-from .post import Post
 from .profile import Profile
-from .order import Order
-from .order_product_association import OrderProductAssociation
+
+# from .post import Post
+# from .order import Order
+# from .order_product_association import OrderProductAssociation
